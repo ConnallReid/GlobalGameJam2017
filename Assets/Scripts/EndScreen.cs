b3b57fe7cc18835bd3a13e0a276ffc05b489153e
Connall <connall@thereidfamily.co.uk>
2017-01-22 07:37:35 +0000

    Big Splash!

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour {
+
+    public float speed = 100f;
+
+    public bool fall = false;
+
+    void Update()
+    {
+        if (GameObject.Find("Timer").GetComponent<Text>().text == "3")
+        {
+            fall = true;
+        }
+        if (fall == true)
+        {
+            transform.position += new Vector3(0.0f, -50 * speed * Time.deltaTime, 0.0f);
+        }
+    }
+}
